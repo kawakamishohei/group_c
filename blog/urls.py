d64125e703014aa7781a5a30132f09bdd1e8e3c4
kawakamishohei <shohei.kawakami@enisias.com>
2023-06-23 09:57:50 +0900

    start4

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,10 @@ from django.contrib.auth.decorators import login_required
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', views.Home.as_view(), name='home'),
+    path('tag/create/', views.TagCreateView.as_view(), name='tag_create'),
+    path('article/create/', views.ArticleCreateView.as_view(), name='article_create'),
+path('article_update/<int:pk>/', views.ArticleUpdate.as_view(), name='article_update'),
+    path('article_delete/<int:pk>/',views.ArticleDelete.as_view(), name='article_delete'),
+    path('comment/create/<int:pk>',views.CommentCreateView.as_view(), name='comment_create'),
 ]
