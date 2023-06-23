4227d9be236537d16f321c4f56ff81b39a8f1eeb
reijirokanesaka <reijiro.kanesaka@enisias.com>
2023-06-23 17:12:29 +0900

    06/23

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
-from .models import Chat
+from .models import Chat, Comment
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
-from .forms import ChatCreateForm
+from .forms import ChatCreateForm, CommentCreateForm
 # Create your views here.
 class ChatListView(generic.ListView):
     model = Chat
@@ -18,3 +18,15 @@ class ChatCreateView(generic.CreateView):
     template_name = 'chat/chat_create.html'
     success_url = reverse_lazy('chat:chat_list')
     form_class = ChatCreateForm
+
+class CommentCreateView(generic.CreateView):
+    model = Comment
+    template_name = 'blog/comment_create.html'
+    form_class = CommentCreateForm
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        chat = Chat.objects.get(pk=self.kwargs['pk'])
+        comment.target = chat
+        form.save()
+        return redirect('chat:chat_detail',pk = chat.pk)
