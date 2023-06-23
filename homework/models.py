aaf04d3bbc3bcc6b59c0f061830a6f4dd54d3268
l1500 <erena.moritaka@enisias.com>
2023-06-23 09:59:07 +0900

    homework4

--- homework/models.py
+++ homework/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('カテゴリ名', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class homework(models.Model):
+    title = models.CharField('教科', max_length=255)
+    text = models.TextField('提出内容')
+    created_at = models.DateTimeField('作成日', default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='カテゴリ')
+
+    def __str__(self):
+        return self.title
+
--- homework/views.py
+++ homework/views.py
@@ -3,4 +3,5 @@ from django.views import generic
 
 class homeworkListView(generic.ListView):
     templates_name = 'homework/homework_list.html'
-# Create your views here.
+
+
