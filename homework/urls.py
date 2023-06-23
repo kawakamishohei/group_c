e5289da7d4c449c98850c9c39f57e4ac77ea2cac
D036-MKM30B4\ia <aaa@aaa.com>
2023-06-23 17:49:00 +0900

    homework7

--- homework/urls.py
+++ homework/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'homework'
 
 urlpatterns = [
-    path('homework/', views.homeworkListView.as_view(), name='homework'),
+    path('homework', views.homeworkListView.as_view(), name='homework'),
+    path('homework_list', views.PostListView.as_view(), name='homework_list')
 ]
--- homework/views.py
+++ homework/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import ListView
+from .models import homework
 
 class homeworkListView(generic.ListView):
     templates_name = 'homework/homework_list.html'
-
+    model = homework
 
