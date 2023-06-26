b7aaf6eaa20d88927a2e9b2177d86035f2ed2c4f
D036-MKM30B4\ia <aaa@aaa.com>
2023-06-26 09:42:13 +0900

    homework8

--- homework/urls.py
+++ homework/urls.py
@@ -5,5 +5,5 @@ app_name = 'homework'
 
 urlpatterns = [
     path('homework', views.homeworkListView.as_view(), name='homework'),
-    path('homework_list', views.PostListView.as_view(), name='homework_list')
+
 ]
