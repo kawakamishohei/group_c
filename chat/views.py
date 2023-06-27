4a1a543f4104cca84abba70ae1e7d4880a03b360
reijirokanesaka <reijiro.kanesaka@enisias.com>
2023-06-27 17:02:14 +0900

    06/27

--- chat/views.py
+++ chat/views.py
@@ -3,25 +3,35 @@ from django.views import generic
 from .models import Chat, Comment
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
-from .forms import ChatCreateForm, CommentCreateForm
+from .forms import ChatCreateForm, CommentCreateForm, SearchForm
+from django.contrib.auth.models import User
+from django.db.models import Q
 # Create your views here.
 class ChatListView(generic.ListView):
     model = Chat
     template_name = 'chat/chat_list.html'
 
-class ChatDetailView(generic.DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = SearchForm(self.request.GET)
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset().order_by('-created_at')
+        form = SearchForm(self.request.GET)
+        form.is_valid()
+        keyword = form.cleaned_data.get('keyword')
+        if keyword:
+            queryset = queryset.filter(
+                # 記事のタイトルか、本文のどちらかにキーワードが含まれているものを絞り込む
+                Q(title__icontains=keyword) | Q(text__icontains=keyword)
+            )
+        return queryset
+
+
+class ChatDetailView(generic.CreateView):
     model = Chat
     template_name = 'chat/chat_detail.html'
-
-class ChatCreateView(generic.CreateView):
-    model = Chat
-    template_name = 'chat/chat_create.html'
-    success_url = reverse_lazy('chat:chat_list')
-    form_class = ChatCreateForm
-
-class CommentCreateView(generic.CreateView):
-    model = Comment
-    template_name = 'blog/comment_create.html'
     form_class = CommentCreateForm
 
     def form_valid(self, form):
@@ -29,9 +39,43 @@ class CommentCreateView(generic.CreateView):
         chat = Chat.objects.get(pk=self.kwargs['pk'])
         comment.target = chat
         form.save()
-        return redirect('chat:chat_detail',pk = chat.pk)
+        return redirect('chat:chat_detail', pk=chat.pk)
 
-class GoodsDelete(generic.DeleteView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['chat'] = Chat.objects.get(pk=self.kwargs['pk'])
+        return context
+
+    def get_initial(self):
+        initial = {}
+        initial['name'] = self.request.user.username
+        return initial
+
+
+class ChatCreateView(generic.CreateView):
+    model = Chat
+    template_name = 'chat/chat_create.html'
+    success_url = reverse_lazy('chat:chat_list')
+    form_class = ChatCreateForm
+
+    def get_initial(self):
+        initial = {}
+        initial['upname'] = self.request.user.username
+        return initial
+
+# class CommentCreateView(generic.CreateView):
+#     model = Comment
+#     template_name = 'chat/comment_create.html'
+#     form_class = CommentCreateForm
+#
+#     def form_valid(self, form):
+#         comment = form.save(commit=False)
+#         chat = Chat.objects.get(pk=self.kwargs['pk'])
+#         comment.target = chat
+#         form.save()
+#         return redirect('chat:chat_detail',pk = chat.pk)
+
+class ChatDelete(generic.DeleteView):
     # フォームは必要なし
     model = Chat
     template_name = 'chat/chat_delete.html'
